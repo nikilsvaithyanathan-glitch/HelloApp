2a43734292f0264fc243348a250bc5a83ef4d6b4
nikilsvaithyanathan <nikilsvaithyanathan@gmail.com>
2026-04-10 09:17:13 +0530

    UC-3 display hello world using command line

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // UC1: If no argument → print Hello World
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } 
+        // UC2: If argument exists → print personalized greeting
+        else {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }
